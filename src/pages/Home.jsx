01e366bd8566992b2894f4a2d977e6a324420c89
pedro marchese <pedro.marchese03@gmail.com>
2025-10-14 21:50:04 -0300

    4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
+const api = import.meta.env.VITE_API
 export default function Home() {
-  
+  useEffect(() => {
+    fetch(api)
+    .then
+  }, [])
   return (
     <div>
         <h2 className="text-2xl font-bold mb-4">Bem-vindo ao Fiap Commerce!</h2>
+        <p>ola</p>
     </div>
   );
 }
